207148127c3fa2eeb896d8adcc2c89204eb7cedb
Vasu Sharma <vsh.9705@gmail.com>
2025-07-03 22:54:31 +0000

    Initial commit

---
+++ chatsystemproj/chatapp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Prefetch
+
+# Create your models here.
+class ConversationManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related(
+            Prefetch('participants', queryset=User.objects.only('id', 'username'))
+        )
+    
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name = 'conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = ConversationManager()
+
+    def __str__(self):
+        participant_names = " ,".join([user.username for user in self.participants.all()])
+        return f'Conversation with {participant_names}'
+    
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Message from {self.sender.username} in {self.content[:20]}'
